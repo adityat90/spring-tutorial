f49566c1ecfbf1178b964d366c85722cf47cccae
Aditya Talpade <adityat90@gmail.com>
2017-10-05 22:50:39 +0530

    Singleton Scope

--- src/main/java/com/adityatalpade/App.java
+++ src/main/java/com/adityatalpade/App.java
@@ -9,7 +9,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
-        Shape shape = applicationContext.getBean("triangle", Shape.class);
-        shape.draw();
+        Shape shape1 = applicationContext.getBean("triangle", Shape.class);
+        Shape shape2 = applicationContext.getBean("triangle", Shape.class);
+        System.out.println("shape1 = " + shape1.hashCode());
+        System.out.println("shape2 = " + shape2.hashCode());
     }
 }
